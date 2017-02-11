33a0b79aa61b600428f8894082d4e60aa57799cf
Elliott Dicus <ewdicus@gmail.com>
2017-02-11 14:05:00 -0800

    Add caching

--- run.py
+++ run.py
@@ -1,12 +1,34 @@
 import os
 import json
+
 from twitter import Twitter
+from cache import get_last_tweet_id, set_last_tweet_id
+
+# Planning to run this once per hour. Doubt he'll spam more than 50 over that
+# length of time. ¯\_(ツ)_/¯
+TWEET_LIMIT = 50
 
 def main():
     twitter = Twitter()
-    new_tweets = twitter.get_new_trump_tweets(limit=10)
+
+    # Check if we already have a tweet id to start from
+    last_tweet_id = get_last_tweet_id()
+
+    # Grab any new tweets
+    new_tweets = twitter.get_new_trump_tweets(limit=TWEET_LIMIT,
+                                              since_id=last_tweet_id)
+
+    # If there aren't any we're done
+    if not new_tweets:
+        print("No new tweets since id:{}".format(last_tweet_id))
+        return
+
+    # Store the new last_tweet_id. Newest first, so it's just the id of the
+    # most recent one.
+    set_last_tweet_id(new_tweets[0].id)
+
     for tweet in new_tweets:
-        print("\"{}\"".format(tweet.text))
+        print("\"{}\" - {}".format(tweet.text, tweet.id))
 
 if __name__ == "__main__":
     main()
