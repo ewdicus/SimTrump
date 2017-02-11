43c7c0024c89df7a9148e62b3a1b4e6d5a136e71
Elliott Dicus <ewdicus@gmail.com>
2017-02-11 14:02:53 -0800

    Helper functions for caching the last tweet id

---
+++ cache.py
@@ -0,0 +1,14 @@
+from iron_cache import *
+
+cache = IronCache()
+
+
+def get_last_tweet_id():
+    try:
+        item = cache.get(cache="main", key="last_tweet_id")
+    except:
+        return None
+    return item.value
+
+def set_last_tweet_id(id):
+    cache.put(cache="main", key="last_tweet_id", value=str(id))
